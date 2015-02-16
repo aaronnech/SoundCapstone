ef43fbc97deec440f23014391dff25b8039bc49d
Aaron Nech <necha@cs.washington.edu>
2015-02-15 22:08:58 -0800

    fixed

---
+++ src/server/config.js
@@ -0,0 +1,8 @@
+/**
+ * Configuration file for the server
+ */
+module.exports = {
+    db : {
+        url : "mongodb://admin:pass@ds043991.mongolab.com:43991/kimbee"
+    }
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,29 @@
-var express = require("express");
+var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
+var config = require('./config');
+
+// Connect to database
+mongoose.connect(config.db.url);
+mongoose.connection.on('error', function(err) {
+    console.log("mongodb error:");
+    console.log(err);
+    process.exit(1);
+});
+
+
+// Express middleware
+app.use(bodyParser.json());
 app.use('/therapist', express.static(__dirname + '/../therapist'));
 app.use('/game', express.static(__dirname + '/../game'));
+
+
+// Setup routes
+var users = require('./routes/users');
+
+app.post('/api/user/register', users.register);
+app.post('/api/user/login', users.login);
 
 app.listen(1337);
 
