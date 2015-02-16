7c657f52e842b39c8374aefec062f946bb3babd9
Aaron Nech <necha@cs.washington.edu>
2015-02-15 16:53:51 -0800

    fixed

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,9 @@
-/**
- * Created by aaron on 2/15/15.
- */
+var express = require("express");
+var app = express();
+
+app.use('/therapist', express.static(__dirname + '/../therapist'));
+app.use('/game', express.static(__dirname + '/../game'));
+
+app.listen(1337);
+
+console.log('Listening on port 1337...');
