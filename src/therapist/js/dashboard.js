4119a2cc443b94b558d59cd7f80b32de438b8798
Ryan Drapeau <drapeau@cs.washington.edu>
2015-02-17 11:43:39 -0800

    Add Child Link

--- src/therapist/js/dashboard.js
+++ src/therapist/js/dashboard.js
@@ -6,5 +6,14 @@ $(function () {
 
     function setup() {
         $("#therapistName").text("THERAPIST NAME");
+        $("#addChildButton").click(addChild);
+    };
+
+    function addChild() {
+        child = {name : $("#childNameInput").val()};
+        console.log(child);
+        $.post("/api/child/add", child, function(data) {
+            console.log(data);
+        });
     };
 });
