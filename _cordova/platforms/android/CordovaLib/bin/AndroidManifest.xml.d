58fbe0a90740ec8ca6fce344dcc9dc083deaf80b
Aaron Nech <necha@cs.washington.edu>
2015-03-10 03:51:34 -0700

    fixed

--- _cordova/platforms/android/CordovaLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/aaron/Dropbox/School/CLASSES/2014-2015/WINTER/CSE481/SoundCapstone/_cordova/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
-/home/aaron/Dropbox/School/CLASSES/2014-2015/WINTER/CSE481/SoundCapstone/_cordova/platforms/android/CordovaLib/AndroidManifest.xml \
--- _cordova/platforms/android/CordovaLib/xwalk_core_library/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/aaron/Dropbox/School/CLASSES/2014-2015/WINTER/CSE481/SoundCapstone/_cordova/platforms/android/CordovaLib/xwalk_core_library/bin/AndroidManifest.xml : \
-/home/aaron/Dropbox/School/CLASSES/2014-2015/WINTER/CSE481/SoundCapstone/_cordova/platforms/android/CordovaLib/xwalk_core_library/AndroidManifest.xml \
